e207526af1f2c25664b3e080b7ea325e759aa42b
Asif Hassan <aoeasif@gmail.com>
2021-01-24 21:32:00 +0600

    resolve typescript dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
 import "./media/styles/base.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<h1>Hello World</h1>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
